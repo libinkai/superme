8491c73fc5abb302d935452cfb06dab16ae5fff1
LIBINKAI <841116931@qq.com>
2020-01-16 07:53:49 +0800

    快慢指针

---
+++ src/main/java/com/leetcode/Problem141.java
@@ -0,0 +1,49 @@
+package com.leetcode;
+
+import java.util.HashSet;
+
+/**
+ * @Author: Equator
+ * @Date: 2020/1/16 7:26
+ **/
+
+public class Problem141 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    // hashset
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode tempNode = head;
+        while (tempNode != null) {
+            if (set.contains(tempNode)) {
+                return true;
+            }
+            set.add(tempNode);
+            tempNode = tempNode.next;
+        }
+        return false;
+    }
+
+    // fast slow pointer 快慢指针
+    public boolean hasCycle1(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head, fast = slow.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
