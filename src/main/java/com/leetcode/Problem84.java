e5f53cf2268cfab2a33f51eb9f8eb1a1c4624fdd
LIBINKAI <841116931@qq.com>
2020-01-18 17:46:56 +0800

    84. 柱状图中最大的矩形

--- src/main/java/com/leetcode/Problem84.java
+++ src/main/java/com/leetcode/Problem84.java
@@ -43,7 +43,16 @@ public class Problem84 {
         int maxArea = 0;
         // 创建一个栈来保存下标
         Stack<Integer> stack = new Stack<>();
-        // stack.push(-1);
+        stack.push(-1);
+        for (int i = 0; i < heights.length; ++i) {
+            while (stack.peek() != -1 && heights[i] <= heights[stack.peek()]) {
+                maxArea = Math.max(maxArea, heights[stack.pop()] * (i - stack.peek() - 1));
+            }
+            stack.push(i);
+        }
+        while (stack.peek() != -1) {
+            maxArea = Math.max(maxArea, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+        }
         return maxArea;
     }
 
