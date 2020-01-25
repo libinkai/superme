9360bf5a7d258822c0a53c78f6d4b3e5048566d2
LIBINKAI <841116931@qq.com>
2020-01-25 20:24:16 +0800

    DP

--- src/main/java/com/equator/algorithm/searching/BinarySearch.java
+++ src/main/java/com/equator/algorithm/searching/BinarySearch.java
@@ -7,10 +7,10 @@ package com.equator.algorithm.searching;
 
 // 二分查找的前提：目标函数的单调性、上下界、可以随机访问（索引访问）
 public class BinarySearch {
-    // 二分查找返回目标的下标
-    public int binarySearch(int[] nums, int target) {
+    // 二分查找返回目标的下标 注意，以下代码适用于升序排列的数组
+    public int binarySearch1(int[] nums, int target) {
         int low = 0, high = nums.length - 1, mid;
-        while (low < high) {
+        while (low <= high) {
             mid = low + (high - low) / 2;
             if (target == nums[mid]) {
                 return mid;
@@ -23,13 +23,29 @@ public class BinarySearch {
         return -1;
     }
 
-    public void test1() {
+    public int binarySearch2(int[] nums, int target) {
+        int low = 0, high = nums.length, mid;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public void test() {
         int[] nums = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
-        int target = 17;
-        System.out.println(binarySearch(nums, target));
+        for (int i = 0; i < nums.length; i++) {
+            System.out.printf("target=%d idx=%d res=%d\n", nums[i], i, binarySearch2(nums, nums[i]));
+        }
     }
 
     public static void main(String[] args) {
-        new BinarySearch().test1();
+        new BinarySearch().test();
     }
 }
---
+++ src/main/java/com/leetcode/Problem509.java
@@ -0,0 +1,42 @@
+package com.leetcode;
+
+/**
+ * @Author: Equator
+ * @Date: 2020/1/25 17:40
+ **/
+
+public class Problem509 {
+    // 傻递归 自顶向下
+    public int fib0(int N) {
+        return N <= 1 ? N : fib0(N - 1) + fib0(N - 2);
+    }
+
+    // 记忆化搜索
+    public int fib1(int N) {
+        int[] memo = new int[N + 1];
+        return fib1(N, memo);
+    }
+
+    public int fib1(int N, int[] memo) {
+        if (N <= 1) {
+            return N;
+        } else if (memo[N] == 0) {
+            memo[N] = fib1(N - 1, memo) + fib1(N - 2, memo);
+        }
+        return memo[N];
+    }
+
+    // 自底向上（动态规划？？？）
+    public int fib2(int N) {
+        if (N <= 1) {
+            return N;
+        }
+        int[] memo = new int[N + 1];
+        memo[0] = 0;
+        memo[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        return memo[N];
+    }
+}
