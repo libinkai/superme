210798170d5b273b97dc1312c2e0f6fbfd9324b5
LIBINKAI <841116931@qq.com>
2020-02-15 16:39:36 +0800

    队列实现栈，栈实现队列

--- src/main/java/com/leetcode/sword/Problem9.java
+++ src/main/java/com/leetcode/sword/Problem9.java
@@ -1,5 +1,8 @@
 package com.leetcode.sword;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -27,6 +30,44 @@ public class Problem9 {
                 }
             }
             return slave.isEmpty() ? -1 : slave.pop();
+        }
+    }
+
+    static class CStack {
+        Deque master, slave;
+
+        public CStack() {
+            master = new LinkedList<>();
+            slave = new LinkedList<>();
+        }
+
+        public void push(int val) {
+            master.add(val);
+        }
+
+        public int pop() {
+            if (master.isEmpty()) {
+                return -1;
+            }
+            while (master.size() > 1) {
+                slave.add(master.removeFirst());
+            }
+            Integer top = (Integer) master.removeFirst();
+            Deque temp = master;
+            master = slave;
+            slave = temp;
+            return top;
+        }
+    }
+
+    public static void main(String[] args) {
+        CStack stack = new CStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(stack.pop());
         }
     }
 }
