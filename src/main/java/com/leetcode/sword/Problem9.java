a1a85ea061e3cd0a9f73589b53ce47c8c9a9ba1c
LIBINKAI <841116931@qq.com>
2020-02-15 15:50:48 +0800

    队列实现栈，栈实现队列

--- src/main/java/com/leetcode/sword/Problem9.java
+++ src/main/java/com/leetcode/sword/Problem9.java
@@ -1,9 +1,32 @@
 package com.leetcode.sword;
 
+import java.util.Stack;
+
 /**
  * @Author: Equator
  * @Date: 2020/2/15 15:17
  **/
 
 public class Problem9 {
+    class CQueue {
+        Stack<Integer> master, slave;
+
+        public CQueue() {
+            master = new Stack<>();
+            slave = new Stack<>();
+        }
+
+        public void appendTail(int value) {
+            master.push(value);
+        }
+
+        public int deleteHead() {
+            if (slave.isEmpty()) {
+                while (!master.isEmpty()) {
+                    slave.push(master.pop());
+                }
+            }
+            return slave.isEmpty() ? -1 : slave.pop();
+        }
+    }
 }
