ec508c639287aae715303fb3850f948fb012c5d0
LIBINKAI <841116931@qq.com>
2020-02-23 07:52:04 +0800

    丑数、第一次出现的字符串（ï字符¼

---
+++ src/main/java/com/equator/leetcode/sword/Problem50.java
@@ -0,0 +1,26 @@
+package com.equator.leetcode.sword;
+
+/**
+ * @Author: Equator
+ * @Date: 2020/2/23 7:44
+ **/
+
+public class Problem50 {
+    public char firstUniqChar(String s) {
+        if (s == null || s.length() == 0) {
+            return ' ';
+        }
+        // 两遍扫描字符串
+        char[] chars = s.toCharArray();
+        int[] map = new int[256];
+        for (int i = 0; i < chars.length; i++) {
+            map[chars[i]]++;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (map[chars[i]] == 1) {
+                return chars[i];
+            }
+        }
+        return ' ';
+    }
+}
