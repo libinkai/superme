2c2da308bc685cb044ea82269d03b5b7d91353d2
LIBINKAI <841116931@qq.com>
2020-02-25 09:55:08 +0800

    位运算

--- src/main/java/com/equator/leetcode/sword/Problem56_1.java
+++ src/main/java/com/equator/leetcode/sword/Problem56_1.java
@@ -7,6 +7,36 @@ package com.equator.leetcode.sword;
 
 public class Problem56_1 {
     public int[] singleNumbers(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return nums;
+        }
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= nums[i];
+        }
+        int idx = getFirstBitIdx(xor);
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (isBit(nums[i], idx)) {
+                res[0] ^= nums[i];
+            } else {
+                res[1] ^= nums[i];
+            }
+        }
+        return res;
+    }
 
+    private boolean isBit(int num, int idx) {
+        num >>= idx;
+        return (num & 1) == 1 ? true : false;
+    }
+
+    private int getFirstBitIdx(int num) {
+        int idx = 0;
+        while (((num & 1) == 0) && idx < 32) {
+            num >>= 1;
+            idx++;
+        }
+        return idx;
     }
 }
