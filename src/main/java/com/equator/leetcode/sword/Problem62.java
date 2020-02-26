705fea7a94ed597874f9c587bb95ffc77ffb9f6e
LIBINKAI <841116931@qq.com>
2020-02-26 10:36:19 +0800

    股票利润环形链表

---
+++ src/main/java/com/equator/leetcode/sword/Problem62.java
@@ -0,0 +1,30 @@
+package com.equator.leetcode.sword;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: Equator
+ * @Date: 2020/2/26 10:05
+ **/
+
+public class Problem62 {
+    public int lastRemaining(int n, int m) {
+        if (n == 0 || m == 0) {
+            return -1;
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        // 第一次删除的数字下标为(m-1)%n记为c，之后每一次删除的数字下标均为(c+m-1)%list.size()
+        int idx = (m - 1) % n;
+        while (list.size() > 1) {
+            list.remove(idx);
+            idx = (idx + m - 1) % list.size();
+        }
+        return list.get(0);
+    }
+}
