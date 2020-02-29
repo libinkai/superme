a049335a1f599ce5bfd13a0ddaafef197d06982c
LIBINKAI <841116931@qq.com>
2020-02-29 18:15:29 +0800

    对称树

---
+++ src/main/java/com/equator/leetcode/Problem101.java
@@ -0,0 +1,53 @@
+package com.equator.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: Equator
+ * @Date: 2020/2/29 17:58
+ **/
+
+public class Problem101 {
+    public boolean isSymmetric(TreeNode root) {
+        Deque<TreeNode> list = new LinkedList<>();
+        list.add(root);
+        list.add(root);
+        while (!list.isEmpty()) {
+            TreeNode node1 = list.poll();
+            TreeNode node2 = list.poll();
+            if (node1 == null && node2 == null) {
+                continue;
+            }
+            if (node1 == null || node2 == null) {
+                return false;
+            }
+            if (node1.val != node2.val) {
+                return false;
+            }
+            list.add(node1.left);
+            list.add(node2.right);
+            list.add(node1.right);
+            list.add(node2.left);
+        }
+        return true;
+    }
+
+    public boolean isSymmetric0(TreeNode root) {
+        return rec(root, root);
+    }
+
+    private boolean rec(TreeNode r1, TreeNode r2) {
+        if (r1 == null && r2 == null) {
+            return true;
+        }
+        if (r1 == null || r2 == null) {
+            return false;
+        }
+        if (r1.val == r2.val) {
+            return rec(r1.left, r2.right) && rec(r1.right, r2.left);
+        }
+        return false;
+    }
+}
