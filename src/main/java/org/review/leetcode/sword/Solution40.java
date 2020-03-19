2ab011ae0b4db026790d5a059d3b829c18d9fe4d
LIBINKAI <841116931@qq.com>
2020-03-20 07:41:00 +0800

    最小的k个数

---
+++ src/main/java/org/review/leetcode/sword/Solution40.java
@@ -0,0 +1,31 @@
+package org.review.leetcode.sword;
+
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+
+/**
+ * @Author: Equator
+ * @Date: 2020/3/20 7:30
+ **/
+
+public class Solution40 {
+    // PriorityQueue 小顶堆
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (arr == null || arr.length == 0) {
+            return new int[0];
+        }
+        if (k <= 0 || arr.length < k) {
+            return new int[0];
+        }
+        PriorityQueue<Integer> heap = new PriorityQueue<>(arr.length);
+        int[] res = new int[k];
+        for (int num : arr) {
+            heap.add(num);
+        }
+        for (int i = 0; i < k; i++) {
+            res[i] = heap.poll();
+        }
+        return res;
+    }
+}
