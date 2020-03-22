d327555815f735ed47f5ffdc7f1726a9f63f5836
LIBINKAI <841116931@qq.com>
2020-03-22 21:49:09 +0800

    二叉树最近公共祖先

--- src/main/java/com/equator/leetcode/Problem236.java
+++ src/main/java/com/equator/leetcode/Problem236.java
@@ -1,6 +1,7 @@
 package com.equator.leetcode;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @Author: Equator
@@ -8,20 +9,16 @@ import java.util.Stack;
  **/
 
 public class Problem236 {
+    // 递归遍历左右子树，如果左右子树查到节点都不为空，则表明p和q分别在左右子树中，因此，当前节点即为最近公共祖先；如果左右子树其中一个不为空，则返回非空节点。
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Stack<TreeNode> pAncestor = new Stack<>();
-        Stack<TreeNode> qAncestor = new Stack<>();
-        return null;
-    }
-
-    public void travel(Stack<TreeNode> ancestor, TreeNode root, TreeNode target) {
-        if (root != null) {
-            if (root.equals(target)) {
-                return;
-            }
-            travel(ancestor, root.left, target);
-            ancestor.add(root);
-            travel(ancestor, root.right, target);
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
         }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
     }
 }
