77b821845f0dede80fd76ce2c912358c7c511d75
LIBINKAI <841116931@qq.com>
2020-03-28 17:06:11 +0800

    字典树 单词压缩

--- src/main/java/com/equator/leetcode/Problem820.java
+++ src/main/java/com/equator/leetcode/Problem820.java
@@ -1,5 +1,7 @@
 package com.equator.leetcode;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
 import java.util.Arrays;
 
 /**
@@ -27,6 +29,29 @@ public class Problem820 {
 
     // 字典树
     public int minimumLengthEncoding1(String[] words) {
+        Arrays.sort(words, (w1, w2) -> w2.length() - w1.length());
+        int len = 0;
+        Trie trie = new Trie();
+        for (String word : words) {
+            // 倒着插入
+            if (!trie.startsWith(reverse(word))) {
+                len += word.length() + 1;
+                trie.insert(reverse(word));
+            }
+        }
+        return len;
+    }
 
+    private String reverse(String str) {
+        char[] chars = str.toCharArray();
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            char t = chars[i];
+            chars[i] = chars[j];
+            chars[j] = t;
+            i++;
+            j--;
+        }
+        return new String(chars);
     }
 }
