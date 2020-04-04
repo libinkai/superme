2d3b36773f5e889027a40057b053ba7a76e95d4e
LIBINKAI <841116931@qq.com>
2020-04-04 21:13:46 +0800

    单调栈 接雨水 抄作业的一天

--- src/main/java/com/equator/leetcode/Problem42.java
+++ src/main/java/com/equator/leetcode/Problem42.java
@@ -8,7 +8,7 @@ import java.util.Stack;
  **/
 
 public class Problem42 {
-    public int trap(int[] height) {
+    public int trap1(int[] height) {
         int capacity = 0, top, w, h;
         // 保存下标
         Stack<Integer> stack = new Stack<>();
@@ -28,10 +28,31 @@ public class Problem42 {
         return capacity;
     }
 
+    public int trap2(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int capacity = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
+                int cur = stack.pop();
+                while (!stack.isEmpty() && height[stack.peek()] == height[cur]) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty()) {
+                    int stackTop = stack.peek();
+                    capacity += (Math.min(height[stackTop], height[i]) - height[cur]) * (i - stackTop - 1);
+                }
+            }
+            stack.push(i);
+        }
+        return capacity;
+    }
+
     public static void main(String[] args) {
         int[] heights = {0, 1, 0, 0, 2};
-        System.out.println(new Problem42().trap(heights));
-        ;
+        System.out.println(new Problem42().trap2(heights));
     }
 
 }
