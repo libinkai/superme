487ccdd3d000d17ead62127c5b8cab12b93044e2
LIBINKAI <841116931@qq.com>
2020-05-02 08:32:46 +0800

    无重复字符的最长子串

---
+++ src/main/java/com/equator/leetcode/round1/Solution3.java
@@ -0,0 +1,60 @@
+package com.equator.leetcode.round1;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: Equator
+ * @Date: 2020/5/2 7:51
+ **/
+
+public class Solution3 {
+    // bbabc
+    public int lengthOfLongestSubstringError(String s) {
+        if ("".equals(s)) {
+            return 0;
+        }
+        char[] chars = s.toCharArray();
+        int[] lens = new int[chars.length];
+        lens[0] = 1;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put(chars[0], 0);
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i < chars.length; i++) {
+            if (map.containsKey(chars[i])) {
+                map = new HashMap<>();
+                lens[i] = 1;
+            } else {
+                lens[i] = lens[i - 1] + 1;
+            }
+            map.put(chars[i], i);
+            max = Math.max(max, lens[i]);
+        }
+        return max;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(lengthOfLongestSubstringError("abcabcbb"));
+        System.out.println(lengthOfLongestSubstringError("bbbbb"));
+        System.out.println(lengthOfLongestSubstringError("pwwkew"));
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int start = -1, maxLen = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            // 字符c在字典中 且 上次出现的下标大于当前长度的起始下标
+            if (map.containsKey(chars[i]) && map.get(chars[i]) > start) {
+                start = map.get(chars[i]);
+            } else {
+                maxLen = Math.max(maxLen, i - start);
+            }
+            map.put(chars[i], i);
+        }
+        return maxLen;
+    }
+}
