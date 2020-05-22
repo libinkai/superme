5f3f1ec9baa02e5c2d0f92943c1eb8cebcd82e79
LIBINKAI <841116931@qq.com>
2020-05-22 15:07:16 +0800

    从前序与中序遍历序列构造二叉树

--- src/main/java/com/equator/leetcode/round1/Problem105.java
+++ src/main/java/com/equator/leetcode/round1/Problem105.java
@@ -1,5 +1,9 @@
 package com.equator.leetcode.round1;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * @Author: Equator
  * @Date: 2020/1/21 20:14
@@ -7,6 +11,46 @@ package com.equator.leetcode.round1;
 
 public class Problem105 {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
+        if (inorder.length <= 0) {
+            return null;
+        }
+        if (inorder.length == 1) {
+            return new TreeNode(inorder[0]);
+        }
+        int rootVal = preorder[0];
+        TreeNode root = new TreeNode(rootVal);
+        int rootIdx = getIndex(inorder, rootVal);
+        int leftSize = rootIdx, rightSize = inorder.length - 1 - leftSize;
+        int[] inorderLeft = Arrays.copyOfRange(inorder, 0, rootIdx);
+        int[] inorderRight = Arrays.copyOfRange(inorder, rootIdx + 1, rootIdx + rightSize + 1);
+        int[] preorderLeft = Arrays.copyOfRange(preorder, 1, leftSize + 1);
+        int[] preorderRight = Arrays.copyOfRange(preorder, 1 + leftSize, leftSize + rightSize + 1);
+        root.left = buildTree(preorderLeft, inorderLeft);
+        root.right = buildTree(preorderRight, inorderRight);
+        return root;
+    }
+
+    private int getIndex(int[] inorder, int rootValue) {
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootValue) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 包头不包尾
+    @Test
+    public void test() {
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(Arrays.copyOfRange(nums, 0, 2)));
+        System.out.println(Arrays.toString(Arrays.copyOfRange(nums, 0, 0)));
+    }
+
+    @Test
+    public void testMain() {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        buildTree(preorder, inorder);
     }
 }
