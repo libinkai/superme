bb547fdca1fd08bdf812ac5af3b07d7cb4d325de
LIBINKAI <841116931@qq.com>
2020-05-26 14:12:01 +0800

    重复的数字

---
+++ src/main/java/com/equator/leetcode/round1/Problem287.java
@@ -0,0 +1,31 @@
+package com.equator.leetcode.round1;
+
+/**
+ * 二分法 抽屉原理
+ *
+ * @Author: Equator
+ * @Date: 2020/5/26 9:06
+ **/
+
+public class Problem287 {
+    public int findDuplicate(int[] nums) {
+        // 不是数组下标而是数字的区间
+        int left = 1, right = nums.length - 1;
+        while (left < right) {
+            // 当 left + right 溢出的时候，无符号右移保证结果依然正确
+            int mid = (left + right) >>> 1;
+            int cnt = 0;
+            for (int num : nums) {
+                if (num <= mid) {
+                    cnt++;
+                }
+            }
+            if (cnt > mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
